4a1253fa8dd37a2099125aecfe19baf13bad8158
Andre <andre.almeida3@gmail.com>
2018-11-26 20:57:47 -0200

    Alvaro's changes
    mostly filling the translation tables

--- src/translator.cpp
+++ src/translator.cpp
@@ -59,48 +59,163 @@ list<Token>::iterator transl_mnemonic(list<Token>::iterator it, char * s){
 		 case OP_ADD      :
 			if (nasmfile.is_open()){
 			 	nasmfile << "add eax, ";
-				it++;
-				nasmfile << it->str << endl;
-				it++;
+                it++;
+                if (it->type == TT_CONST) {
+                    nasmfile << it->str << endl;
+                }else{
+                    nasmfile << "[" << it->str << "]" << endl;
+                }
+                it++;
+
 			}else{
 		 		cout << "Falha na criação ou abertura do arquivo." << endl;
 		 		exit(EXIT_FAILURE);
 			}
+
 		 break;
 		 case OP_SUB      :
 		 if (nasmfile.is_open()){
 			nasmfile << "sub eax, ";
-			it++;
-			nasmfile << it->str << endl;
-			it++;
-		}else{
+             it++;
+             if (it->type == TT_CONST) {
+                 nasmfile << it->str << endl;
+             }else{
+                 nasmfile << "[" << it->str << "]" << endl;
+             }
+             it++;
+
+         }else{
 			cout << "Falha na criação ou abertura do arquivo." << endl;
 			exit(EXIT_FAILURE);
 		}
+
 		 break;
 		 case OP_MULT     :
+			 if (nasmfile.is_open()){
+				 nasmfile << "imul ";
+				 it++;
+				 if (it->type == TT_CONST) {
+				     nasmfile << it->str << endl;
+				 }else{
+				     nasmfile << "DWORD[" << it->str << "]" << endl;
+                 }
+				 it++;
+
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 		 break;
 		 case OP_DIV      :
+			 if (nasmfile.is_open()){
+				 nasmfile << "idiv ";
+                 it++;
+                 if (it->type == TT_CONST) {
+                     nasmfile << it->str << endl;
+                 }else{
+                     nasmfile << "DWORD[" << it->str << "]" << endl;
+                 }
+                 it++;
+
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
+
 
 		 break;
 		 case OP_JMP      :
+			 if (nasmfile.is_open()){
+				 nasmfile << "jmp ";
+				 it++;
+				 nasmfile << it->str << endl;
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 		 break;
 		 case OP_JMPN     :
+			 if (nasmfile.is_open()){
+				 nasmfile << "cmp eax, 0" << endl;
+				 nasmfile << "jb" << endl;
+				 it++;
+				 nasmfile << it->str << endl;
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
+
 
 		 break;
 		 case OP_JMPP     :
+			 if (nasmfile.is_open()){
+				 nasmfile << "cmp eax, 0" << endl;
+				 nasmfile << "ja" << endl;
+				 it++;
+				 nasmfile << it->str << endl;
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 		 break;
 		 case OP_JMPZ     :
+			 if (nasmfile.is_open()){
+				 nasmfile << "cmp eax, 0" << endl;
+				 nasmfile << "je" << endl;
+				 it++;
+				 nasmfile << it->str << endl;
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 		 break;
 		 case OP_COPY     :
+			 if (nasmfile.is_open()){
+				 nasmfile << "mov ";
+                 it++;
+                 if (it->type == TT_CONST || it->str == "eax" || it->str == "ebx" || it->str == "ecx" || it->str == "edx" ) {
+                     nasmfile << it->str;
+                 }else{
+                     nasmfile << "[" << it->str << "]";
+                 }
 
-		 break;
+                 nasmfile << " , ";
+				 it++;
+				 it++;
+                 if (it->type == TT_CONST || it->str == "eax" || it->str == "ebx" || it->str == "ecx" || it->str == "edx" ) {
+                     nasmfile << it->str << endl;
+                 }else{
+                     nasmfile << "[" << it->str << "]" << endl;
+                 }
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
+
+			break;
 		 case OP_LOAD     :
-
+			 if (nasmfile.is_open()){
+				 nasmfile << "mov eax, ";
+				 it++;
+                 if (it->type == TT_CONST || it->str == "eax" || it->str == "ebx" || it->str == "ecx" || it->str == "edx" ) {
+                     nasmfile << it->str << endl;
+                 }else{
+                     nasmfile << "[" << it->str << "]" << endl;
+                 }
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 		 break;
 		 case OP_STORE    :
@@ -108,31 +223,192 @@ list<Token>::iterator transl_mnemonic(list<Token>::iterator it, char * s){
 
 		 break;
 		 case OP_INPUT    :
+			 if (nasmfile.is_open()){
+				 nasmfile << "push eax"<< endl;
+				 nasmfile << "push ebx"<< endl;
+				 nasmfile << "push ecx"<< endl;
+				 nasmfile << "push edx"<< endl;
+
+				 nasmfile << "mov eax, 3"<< endl;
+				 nasmfile << "mov ebx, 1"<< endl;
+				 nasmfile << "mov ecx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+
+				 nasmfile << "mov edx, 1" << endl;
+				 nasmfile << "int 0x80"<< endl;
+
+				 nasmfile << "pop edx"<< endl;
+				 nasmfile << "pop ecx"<< endl;
+				 nasmfile << "pop ebx"<< endl;
+				 nasmfile << "pop eax"<< endl;
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 
 		 break;
 		 case OP_OUTPUT   :
+			 if (nasmfile.is_open()){
+				 nasmfile << "push eax"<< endl;
+				 nasmfile << "push ebx"<< endl;
+				 nasmfile << "push ecx"<< endl;
+				 nasmfile << "push edx"<< endl;
+
+				 nasmfile << "mov eax, 4"<< endl;
+				 nasmfile << "mov ebx, 1"<< endl;
+				 nasmfile << "mov ecx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+
+				 nasmfile << "mov edx, 1" << endl;
+				 nasmfile << "int 0x80"<< endl;
+
+				 nasmfile << "pop edx"<< endl;
+				 nasmfile << "pop ecx"<< endl;
+				 nasmfile << "pop ebx"<< endl;
+				 nasmfile << "pop eax"<< endl;
+				 it++;
+
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 
-		 break;
+
+			break;
 		 case OP_C_INPUT  :
+			 if (nasmfile.is_open()){
+				 nasmfile << "push eax"<< endl;
+				 nasmfile << "push ebx"<< endl;
+				 nasmfile << "push ecx"<< endl;
+				 nasmfile << "push edx"<< endl;
+
+				 nasmfile << "mov eax, 3"<< endl;
+				 nasmfile << "mov ebx, 1"<< endl;
+				 nasmfile << "mov ecx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+
+				 nasmfile << "mov edx, 1" << endl;
+				 nasmfile << "int 0x80"<< endl;
+
+				 nasmfile << "pop edx"<< endl;
+				 nasmfile << "pop ecx"<< endl;
+				 nasmfile << "pop ebx"<< endl;
+				 nasmfile << "pop eax"<< endl;
+				 it++;
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 
-		 break;
+			break;
 		 case OP_C_OUTPUT :
+			 if (nasmfile.is_open()){
+				 nasmfile << "push eax"<< endl;
+				 nasmfile << "push ebx"<< endl;
+				 nasmfile << "push ecx"<< endl;
+				 nasmfile << "push edx"<< endl;
+
+				 nasmfile << "mov eax, 4"<< endl;
+				 nasmfile << "mov ebx, 1"<< endl;
+				 nasmfile << "mov ecx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+
+				 nasmfile << "mov edx, 1" << endl;
+				 nasmfile << "int 0x80"<< endl;
+
+				 nasmfile << "pop edx"<< endl;
+				 nasmfile << "pop ecx"<< endl;
+				 nasmfile << "pop ebx"<< endl;
+				 nasmfile << "pop eax"<< endl;
+				 it++;
+
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 
-		 break;
+			break;
 		 case OP_S_INPUT  :
+			 if (nasmfile.is_open()){
+				 nasmfile << "push eax"<< endl;
+				 nasmfile << "push ebx"<< endl;
+				 nasmfile << "push ecx"<< endl;
+				 nasmfile << "push edx"<< endl;
+
+				 nasmfile << "mov eax, 3"<< endl;
+				 nasmfile << "mov ebx, 1"<< endl;
+				 nasmfile << "mov ecx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+
+				 nasmfile << "mov edx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+				 it++;
+				 nasmfile << "int 0x80"<< endl;
+
+				 nasmfile << "pop edx"<< endl;
+				 nasmfile << "pop ecx"<< endl;
+				 nasmfile << "pop ebx"<< endl;
+				 nasmfile << "pop eax"<< endl;
+
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 
 		 break;
 		 case OP_S_OUTPUT :
+			 if (nasmfile.is_open()){
+				 nasmfile << "push eax"<< endl;
+				 nasmfile << "push ebx"<< endl;
+				 nasmfile << "push ecx"<< endl;
+				 nasmfile << "push edx"<< endl;
+
+				 nasmfile << "mov eax, 4"<< endl;
+				 nasmfile << "mov ebx, 1"<< endl;
+				 nasmfile << "mov ecx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+
+				 nasmfile << "mov edx, ";
+				 it++;
+				 nasmfile << it->str << endl;
+				 it++;
+				 nasmfile << "int 0x80"<< endl;
+
+				 nasmfile << "pop edx"<< endl;
+				 nasmfile << "pop ecx"<< endl;
+				 nasmfile << "pop ebx"<< endl;
+				 nasmfile << "pop eax"<< endl;
+
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 
-		 break;
+			break;
 		 case OP_STOP     :
+			 if (nasmfile.is_open()){
+				 nasmfile << "mov eax, 1"<< endl;
+				 nasmfile << "mov ebx, 0"<< endl;
+				 nasmfile << "int 0x80"<< endl;
 
+			 }else{
+				 cout << "Falha na criação ou abertura do arquivo." << endl;
+				 exit(EXIT_FAILURE);
+			 }
 
 		 break;
 		 case OP_BASIC_OP :      //"+, -, /, *, %"
@@ -174,9 +450,15 @@ list<Token>::iterator transl_directive(list<Token>::iterator it, char * s){
 			it ++;
 		break;
 		case DIR_SPACE :
+            nasmfile << "resw 1" << endl;
+            it++;
 
 		break;
 		case DIR_CONST :
+            nasmfile << "dw ";
+            it++;
+            nasmfile << it->str << endl;
+            it++;
 
 		break;
 		case DIR_EQU :
@@ -193,7 +475,7 @@ list<Token>::iterator transl_directive(list<Token>::iterator it, char * s){
 		break;
 		case DIR_TEXT :
 			if (nasmfile.is_open())
-				nasmfile << ".text" << endl << "_start: ";
+				nasmfile << ".text" << endl << "_start: " << endl;
 			else{
 				cout << "Falha na criação ou abertura do arquivo." << endl;
 				exit(EXIT_FAILURE);
